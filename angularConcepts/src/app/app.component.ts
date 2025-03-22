57c95ba7624f70ac2776a91bb4a570662096d18c
SenKanna43 <senthamaraikannanp@gmail.com>
2025-03-22 18:25:02 +0530

    sidebar

--- angularConcepts/src/app/app.component.ts
+++ angularConcepts/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { CartComponent } from "./signals/cart/cart.component";
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CartComponent],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angularConcepts/src/app/app.routes.ts
+++ angularConcepts/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { EmployeeComponent } from './interpolation/employee/employee.component';
+import { ToggleComponent } from './Event Binding/toggle/toggle.component';
+import { UserComponent } from './Two-way Data Binding/user/user.component';
+import { LoadingComponent } from './ngIf structural directive/loading/loading.component';
+import { ProductListComponent } from './ngFor structural directive/product-list/product-list.component';
+import { ProfileComponent } from './Attribute Directive/profile/profile.component';
+import { LoginComponent } from './Custom Directive/login/login.component';
+import { CartComponent } from './signals/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'employee', component: EmployeeComponent},
+    { path: 'toggle', component: ToggleComponent },
+    { path: 'user', component: UserComponent },
+    { path: 'loading', component: LoadingComponent },
+    { path: 'product', component: ProductListComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'cart', component: CartComponent },
+    { path: '', redirectTo: 'toggle', pathMatch: 'full' } // Default route
+  ];
